7e95390322969dfd8eabb30b37e31b4e4122a8ba
DESKTOP-KNEF2HR <stranniknochi@mail.ru>
2022-12-24 09:53:15 +0800

    first commit

---
+++ Zadacha43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43 (ДОП, по желанию, на 5 нужно сделать 2 задачки): Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int num;
+        Console.Write("Введите целое число: ");
+        num = Convert.ToInt32(Console.ReadLine()!);
+        int decimalnum;
+        string rem = "";
+        while (num >= 1)
+        {
+            decimalnum = num / 2;
+            rem += (num % 2).ToString();
+            num = decimalnum;
+        }
+        string bin = "";
+        for (int i = rem.Length - 1; i >= 0; i--)
+        {
+            bin = bin + rem[i];
+        }
+        Console.WriteLine($"Двоичное число : {bin}");
